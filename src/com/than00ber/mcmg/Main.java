ab3180dc3a36f81d7c616c666016a565645c4799
Noah Greff <noahgreff@gmail.com>
2022-02-15 15:43:18 +0100

    Initial commit

---
+++ src/com/than00ber/mcmg/Main.java
@@ -0,0 +1,16 @@
+package com.than00ber.mcmg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
