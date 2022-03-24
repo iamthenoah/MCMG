58367ceeee20edc0d0506f17e249db78fa554be5
Noah Greff <greffnoah@gmail.com>
2022-03-24 12:05:31 +0100

    'MiniGameHandler' is now anonymous

--- src/main/java/com/than00ber/mcmg/game/MiniGameEngine.java
+++ src/main/java/com/than00ber/mcmg/game/MiniGameEngine.java
@@ -231,4 +231,31 @@ public class MiniGameEngine<G extends MiniGame> {
 
         @Nullable Integer getDurationRound();
     }
+
+    private abstract static class MiniGameHandler implements Runnable {
+
+        private final Main instance;
+        private int id;
+
+        protected MiniGameHandler(Main instance) {
+            this.instance = instance;
+        }
+
+        public void activate() {
+            onActivate();
+            id = Bukkit.getScheduler().scheduleSyncRepeatingTask(instance, this, 0, 20);
+        }
+
+        public void deactivate() {
+            onDeactivate();
+            Bukkit.getScheduler().cancelTask(id);
+        }
+
+        @Override
+        public abstract void run();
+
+        public abstract void onActivate();
+
+        public abstract void onDeactivate();
+    }
 }
