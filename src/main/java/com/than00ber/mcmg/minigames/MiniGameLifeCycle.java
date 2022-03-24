ddc86f4db3a19aaba8bf27deac8281fae7fbeb97
Noah Greff <greffnoah@gmail.com>
2022-03-24 16:44:12 +0100

    moved MiniGameLifeCycle

--- src/main/java/com/than00ber/mcmg/minigames/MiniGameLifeCycle.java
+++ src/main/java/com/than00ber/mcmg/minigames/MiniGameLifeCycle.java
@@ -1,5 +1,6 @@
-package com.than00ber.mcmg;
+package com.than00ber.mcmg.minigames;
 
+import com.than00ber.mcmg.MiniGameEvent;
 import com.than00ber.mcmg.objects.WinCondition;
 import org.bukkit.entity.Player;
 
