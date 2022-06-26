4e9ce52ce593ca7680f001f16b393658374b22e7
Noah Greff <greffnoah@gmail.com>
2022-06-26 18:44:32 +0200

    Registry is no longer `Configurable`

--- src/main/java/com/than00ber/mcmg/init/Registry.java
+++ src/main/java/com/than00ber/mcmg/init/Registry.java
@@ -3,17 +3,15 @@ package com.than00ber.mcmg.init;
 import com.google.common.collect.ImmutableList;
 import com.than00ber.mcmg.Main;
 import com.than00ber.mcmg.util.Console;
-import com.than00ber.mcmg.util.config.ConfigProperty;
 import com.than00ber.mcmg.util.config.ConfigUtil;
 import com.than00ber.mcmg.util.config.Configurable;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.function.Supplier;
 
-public final class Registry<E> implements Configurable {
+public final class Registry<E> {
 
     private final Registries registry;
     private final HashMap<String, Supplier<E>> ENTRIES = new HashMap<>();
@@ -42,34 +40,60 @@ public final class Registry<E> implements Configurable {
     }
 
     public void load(Main instance) {
-        Console.debug("Loading '" + registry + "' registry...");
-        ConfigUtil.loadConfigs(instance, this);
-        Console.debug("Done.");
+        Console.debug("Loading '" + registry + "' registry.");
+        ENTRIES.forEach((key, obj) -> {
+            if (obj.get() instanceof Configurable configurable) {
+                String path = registry.name().toLowerCase() + "/" + configurable.getConfigName();
+                YamlConfiguration data = ConfigUtil.load(instance, path);
+                configurable.setConfig(data);
+            }
+        });
     }
 
     public void unload(Main instance) {
-        Console.debug("Unloading '" + registry + "' registry...");
-        ConfigUtil.saveConfigs(instance, this);
-        Console.debug("Done.");
-    }
-
-    @Override
-    public String getConfigName() {
-        return registry.name().toLowerCase() + "-registry";
-    }
-
-    @Override
-    public List<? extends ConfigProperty<?>> getProperties() {
-        List<ConfigProperty<?>> configs = new ArrayList<>();
-        for (Supplier<?> obj : ENTRIES.values()) {
+        Console.debug("Unloading '" + registry + "' registry.");
+        ENTRIES.forEach((key, obj) -> {
             if (obj.get() instanceof Configurable configurable) {
-                configs.addAll(configurable.getProperties());
+                String path = registry.name().toLowerCase() + "/" + configurable.getConfigName();
+                ConfigUtil.save(instance, path, configurable.getConfig());
             }
-        }
-        return configs;
+        });
     }
 
     public enum Registries {
         MINIGAMES, ITEMS, TEAMS
     }
+
+//    public String getRegistryName() {
+//        return registry.name().toLowerCase() + "-registry";
+//    }
+//
+//    public void load(Main instance) {
+//        Console.debug("Loading '" + registry + "' registry from '" + getRegistryName() + "' file.");
+//        YamlConfiguration configs =  ConfigUtil.load(instance, getRegistryName());
+//        ENTRIES.forEach((key, obj) -> {
+//            if (obj.get() instanceof Configurable configurable) {
+//                for (ConfigProperty<?> property : configurable.getProperties()) {
+//                    ConfigurationSection section = configs.getConfigurationSection(key);
+//                    if (section == null) section = configs.createSection(key);
+//                    property.load(section);
+//                }
+//            }
+//        });
+//    }
+//
+//    public void unload(Main instance) {
+//        Console.debug("Unloading '" + registry + "' registry to '" + getRegistryName() + "' file.");
+//        YamlConfiguration configs = new YamlConfiguration();
+//        ENTRIES.forEach((key, obj) -> {
+//            if (obj.get() instanceof Configurable configurable) {
+//                for (ConfigProperty<?> property : configurable.getProperties()) {
+//                    ConfigurationSection section = configs.getConfigurationSection(key);
+//                    if (section == null) section = configs.createSection(key);
+//                    property.save(section);
+//                }
+//            }
+//        });
+//        ConfigUtil.save(instance, getRegistryName(), configs);
+//    }
 }
